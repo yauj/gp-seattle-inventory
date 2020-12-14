c8247d6957316a3a8768ddd38d93e83da86760fc
Jeremy Yau <jeremyyau@GP-Sound-Macbook-Air.local>
2020-12-14 14:49:21 +0800

    Fix dom.Value syntax

--- src/handlers/add-item.ts
+++ src/handlers/add-item.ts
@@ -1,12 +1,14 @@
 /**
  * Adds item to inventory.
  */
+import { Result } from "amazon-qldb-driver-nodejs";
+import { dom } from "ion-js";
 import {executeLambda, insertItem, queryByName, updateItemByName} from "../qldb/apis";
 
 export const handler = async (event: any): Promise<any> => {
     executeLambda(async (tx) => {
-        queryByName(tx, event.name).then((result) => {
-            var resultList = result.getResultList();
+        queryByName(tx, event.name).then((result: Result) => {
+            var resultList: dom.Value[] = result.getResultList();
             if (resultList.length === 0) {
                 // Record doesn't exist. Create new document.
                 var doc = {
@@ -18,8 +20,8 @@ export const handler = async (event: any): Promise<any> => {
                 return insertItem(tx, doc);
             } else {
                 // Record already exists, update locations.
-                var locations = resultList[0].get("locations");
-                locations.push(event.location);
+                var locations: dom.Value[] = resultList[0].get("locations").elements();
+                locations.push(dom.Value.from(event.location));
                 return updateItemByName(tx, "locations", locations, event.name);
             }
         })
