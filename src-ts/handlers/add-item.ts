71653626f6e6ed41c533d9d81affb76e2ed05238
Jeremy Yau <jeremyyau@GP-Sound-Macbook-Air.local>
2020-12-16 10:32:01 +0800

    Simplify add item for now

--- src-ts/handlers/add-item.ts
+++ src-ts/handlers/add-item.ts
@@ -7,27 +7,13 @@ import { executeLambda, insertItem, queryByName, updateItemByName } from "../qld
 
 export const handler = async (event: any): Promise<any> => {
     const returnVal: Result = await executeLambda(async (tx: TransactionExecutor) => {
-        return queryByName(tx, event.name).then((result: Result) => {
-            console.log(result);
-            var resultList: dom.Value[] = result.getResultList();
-            if (resultList.length === 0) {
-                // Record doesn't exist. Create new document.
-                console.log("Attempting to insert item.");
-                var doc: Record<string, any> = {
-                    "name": event.name,
-                    "locations": [event.location],
-                    "tags": event.tags,
-                    "notes": event.notes
-                };
-                return insertItem(tx, doc);
-            } else {
-                // Record already exists, update locations.
-                console.log("Attempting to update locations.");
-                var locations: dom.Value[] = resultList[0].get("locations").elements();
-                locations.push(dom.Value.from(event.location));
-                return updateItemByName(tx, "locations", locations, event.name);
-            }
-        })
+        var doc: Record<string, any> = {
+            "name": event.name,
+            "locations": [event.location],
+            "tags": event.tags,
+            "notes": event.notes
+        };
+        return insertItem(tx, doc);
     });
 
     return {
