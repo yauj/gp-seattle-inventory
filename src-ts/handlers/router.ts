575791f9e9a0a64e36b3ee2ed246ed31d4ab582f
Jeremy Yau <jeremyyau@GP-Sound-Macbook-Air.local>
2020-12-19 11:07:18 +0800

    Fix pinpoint

--- src-ts/handlers/router.ts
+++ src-ts/handlers/router.ts
@@ -2,16 +2,17 @@ import { addDescriptionRouter } from "./add-description"
 import { addItemRouter } from "./add-item";
 import { stringPromise } from "../utility";
 import { deleteTransaction, getTransaction } from "../ddb/apis"
-import { SNSEvent, SNSEventRecord, SNSHandler } from "aws-lambda"
+import { SNSEvent, SNSEventRecord, SNSHandler, Context } from "aws-lambda"
 import { AWSError, Pinpoint } from "aws-sdk"
 import { GetItemOutput } from "aws-sdk/clients/dynamodb"
 
-const pinpoint = new Pinpoint()
+const PINPOINT_APP_ID: string = "0ca91d5a35c8404cbfc39fa4d2818092"
+const pinpoint: Pinpoint = new Pinpoint()
 
 /**
  * Main entry handler, which splits up the records to be handled separately.
  */
-export const handler: SNSHandler = async (event: SNSEvent) => {
+export const handler: SNSHandler = async (event: SNSEvent, context: Context) => {
     await Promise.all(event.Records.map(await processRecord))
 }
 
@@ -22,12 +23,15 @@ function processRecord(record: SNSEventRecord): Promise<any> {
     var msg = JSON.parse(record.Sns.Message)
     var msgBody: string = msg.messageBody.toLowerCase()
 
+    var responseOrigination = msg.destinationNumber
+    var responseDestination = msg.originationNumber
+
     console.log(record.Sns.Message)
 
-    return getTransaction(msg.originationNumber)
-        .then((txEntry: GetItemOutput) => routeRequest(msg.originationNumber, txEntry, msgBody))
+    return getTransaction(responseDestination)
+        .then((txEntry: GetItemOutput) => routeRequest(responseDestination, txEntry, msgBody))
         .catch(logDynamoDBError)
-        .then(sendMessage(msg.originationNumber))
+        .then(sendMessage(responseOrigination, responseDestination))
 }
 
 function routeRequest(number: string, txEntry: GetItemOutput, msgBody: string): Promise<string> {
@@ -62,13 +66,13 @@ function routeRequest(number: string, txEntry: GetItemOutput, msgBody: string): 
  * Function to send Pinpoint response. This is passed down to callbacks, and is the future that is
  * tracked by the top level lambda method, to ensure that all callbacks have been called.
  */
-function sendMessage(number: string): (body: string) => Promise<any> {
+function sendMessage(originationNumber: string, destinationNumber: string): (body: string) => Promise<any> {
     return (body: string) => {
         var params: Pinpoint.Types.SendMessagesRequest = {
-            ApplicationId: process.env.ApplicationId,
+            ApplicationId: PINPOINT_APP_ID,
             MessageRequest: {
                 Addresses: {
-                    [number]: {
+                    [destinationNumber]: {
                         ChannelType: 'SMS'
                     }
                 },
@@ -76,7 +80,8 @@ function sendMessage(number: string): (body: string) => Promise<any> {
                     SMSMessage: {
                         Body: body,
                         MessageType: 'PROMOTIONAL',
-                        OriginationNumber: number
+                        OriginationNumber: originationNumber,
+                        SenderId: 'GP Seattle Inventory'
                     }
                 }
             }
@@ -84,7 +89,7 @@ function sendMessage(number: string): (body: string) => Promise<any> {
     
         return pinpoint.sendMessages(params, (err: AWSError, _: Pinpoint.SendMessagesResponse) => {
             if (err) {
-                console.error("Error encountered when attempting to send to " + number + "\n" + err.message)
+                console.error("Error encountered when attempting to send to " + destinationNumber + "\n" + err.message)
             }
         }).promise()
     }
